c3f63a0975a5f28b08b66578e010735a02098097
Amit Thing <amittamang423@gmail.com>
2022-04-23 17:00:04 +0545

    monster app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react'
+import { CardList } from './components/card-list/card-list.component';
 
-function App() {
-  return (
+
+///////1. using function for app
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           hello everyone
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+
+
+//2. using classes for app
+
+// class App extends Component {
+//   constructor(){
+//     super();
+//     this.state={
+//       string:'Amit Thing'
+//     }
+//   }
+//   render(){
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Hello {this.state.string}
+//         </p>
+//      <button onClick={()=>this.setState({string:'sir'})}>click me</button>
+//       </header>
+//     </div>
+//   );   
+//   }
+// }
+
+
+//3. creating superhero app
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      superHero:[]
+    }
+  }
+
+  async componentDidMount(){
+    const users = await (await fetch('https://jsonplaceholder.typicode.com/users')).json()
+    this.setState({superHero:users})
+    // console.log(users)
+    // .then(response => response.json())
+    // .then(data=>console.log(data))
+  }
+
+  render(){
+    return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <CardList monsters = {this.state.superHero} />
     </div>
-  );
+    )    
+  }
 }
+
 
 export default App;
---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './card.style.css'
+
+export const Card = (props)=>{
+    // console.log(props)
+    return( 
+        <div className="card-container">
+            <img alt="monster" src={`https://robohash.org/${props.hero.id}&size=180x180`}></img>
+            <h1 key={props.hero.id}>I am {props.hero.name}</h1>
+            <h2>{props.hero.email}</h2>
+        </div> )
+}
